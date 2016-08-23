71db98d5d75867b625b25572eb36a48f59ec0501
Simson Garfinkel (FED) <simsong@nist.gov>
2016-08-22 20:42:17 -0400

    updated files for commit

--- tlsa/README.md
+++ tlsa/README.md
@@ -2,5 +2,10 @@ Tester for HTTP and SMTP servers that are protected by TLSA records.
 
 Tester designed to work at command line and as web CGI.
 
-Requires getdns
+Requires:
+ *  getdns
+ * M2Crypto (Python 2.7 version)
+ * openssl 1.0.2 or above.
+
+
 
--- tlsa/centos7-setup.bash
+++ tlsa/centos7-setup.bash
@@ -3,12 +3,23 @@
 # configure centos7 to run the script
 # note: this uses M2Crypto, so we are stuck with Python2
 
-yum -y install python-setuptools openssl-devel getdns getdns-devel pytest m2crypto
+yum -y install python-setuptools openssl-devel getdns getdns-devel pytest m2crypto \
+    httpd mod_ssl
+apachectl start			# start the web server
 
-# Install getdns python if it is not present
-make /usr/lib64/python2.7/site-packages/getdns.so
-make get_altnames
+make /usr/lib64/python2.7/site-packages/getdns.so	# getdns for python2
+make /usr/local/ssl/bin/openssl				# openssl 1.0.2h 
+make get_altnames					# make sure that it's running
+
+
+# make sure firewall is up
+firewall-cmd --add-service=http               2>&1 | grep -v 'Warning: ALREADY_ENABLED'
+firewall-cmd --permanent --add-service=http   2>&1 | grep -v 'Warning: ALREADY_ENABLED'
+firewall-cmd --add-service=https              2>&1 | grep -v 'Warning: ALREADY_ENABLED'
+firewall-cmd --permanent --add-service=https  2>&1 | grep -v 'Warning: ALREADY_ENABLED'
+
 
 # and test it!
 py.test
+
 
