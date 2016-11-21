9893510382755a9e0e417773a04e9da31edd8724
Simson Garfinkel (FED) <simsong@nist.gov>
2016-11-21 13:08:39 -0500

    fixing assertions in test

--- email/test_dbdns.py
+++ email/test_dbdns.py
@@ -8,43 +8,50 @@ import dbdns
 import dns.rdatatype
 
 def test_cname_read():
-    """This test makes use of the fact that a.nitroba.org is set as a cname to b.nitroba.org"""
+    # This test makes use of the fact that a.nitroba.org is set as a cname to b.nitroba.org
     qname = "a.nitroba.org"
-    print("testing",qname)
     T = Tester()
     T.newtest(testname="py.test")
-    response = dbdns.query(T,qname,"CNAME")
-    for rr in response.answer:
-        print("{} rdclass={} rdtype={}".format(rr,rr.rdclass,rr.rdtype))
-        if(rr.rdtype==dns.rdatatype.MX):
-            print("cname for a.nitroba.org is {}".format(rr.target))
-    print("")
+    response = dbdns.query(T,qname,dns.rdatatype.CNAME)
+    count = 0
+    for rset in response.answer:
+        for rr in rset:
+            if(rr.rdtype==dns.rdatatype.CNAME):
+                print(dir(rr))
+                print("cname for a.nitroba.org is {}".format(rr.target))
+                assert(str(rr.target)=='b.nitroba.org.')
+                count += 1
+    assert(count>0)               # no response?
 
-def test_two_A_responses():
-    qname = "dualstack.mc-12555-1019789594.us-east-1.elb.amazonaws.com."
-    print("test_two_A_responses",qname)
-
-    print("let's do this manually first")
-    request = dns.message.make_query(qname, dns.rdatatype.A, want_dnssec=True)
-    response = dns.query.udp(request,dbdns.nsaddr)
-    response = dns.message.from_text(response.to_text())
-    for rrset in response.answer:
-        for rr in rrset:
-            if rr.rdtype == dns.rdatatype.A:
-                print(rr.address)
-
-    print("now test with dbdns")
+def test_a_read():
+    qname = "google-public-dns-a.google.com."
     T = Tester()
     T.newtest(testname="py.test")
     response = dbdns.query(T,qname,dns.rdatatype.A)
+    count = 0
+    for rrset in response.answer:
+        for rr in rrset:
+            if(rr.rdtype==dns.rdatatype.A):
+                print("IP addr for {} is {}".format(qname,rr.address))
+                assert(rr.address=='8.8.8.8')
+                count += 1
+    assert(count>0)
+
+def test_two_A_responses():
+    qname = "dualstack.mc-12555-1019789594.us-east-1.elb.amazonaws.com."
+    T = Tester()
+    T.newtest(testname="py.test")
+    response = dbdns.query(T,qname,dns.rdatatype.A)
+    count = 0
     for rrset in response.answer:
         for rr in rrset:
             if rr.rdtype == dns.rdatatype.A:
                 print(rr.address)
-
-    print("")
+                count +=1
+    assert(count>=2)
     
 
 if __name__=="__main__":
     test_cname_read()
+    test_a_read()
     test_two_A_responses()
